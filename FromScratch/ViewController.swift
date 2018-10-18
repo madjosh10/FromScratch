f665cea6c15dda1d09bbd98f86c49caeb8ab76f6
Joshua Madrigal <madrigaljosh10@yahoo.com>
2018-10-18 00:23:29 -0500

    AR Template from Scratch

--- FromScratch/ViewController.swift
+++ FromScratch/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
